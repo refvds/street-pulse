b643bb5fe663e2998d299ddadc36247ca41c3f03
refvds <refvdsk@gmail.com>
2024-10-18 22:09:40 -0700

    [LAYOUT]: add grid section

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,17 @@ export default function Home() {
       <Section className="mt-20" title="New Arrival">
         <ColumnSlide />
       </Section>
+      <Section className="mt-24" title='Big Saving Zone'>
+      <div className="grid grid-cols-6 grid-rows-6 gap-4">
+          <div className="col-span-2 row-span-3 ">
+            <img src="/assets/41.png" className="object-cover w-full h-full" alt="" />
+          </div>
+          <div className="col-span-2 row-span-3 col-start-3 "> <img src="/assets/42.png" className="object-cover w-full h-full" alt="" /></div>
+          <div className="col-span-2 row-span-3 col-start-5 "> <img src="/assets/43.png" className="object-cover w-full h-full" alt="" /></div>
+          <div className="col-span-3 row-span-3 row-start-4 "> <img src="/assets/44.png"className="object-cover w-full h-full" alt="" /></div>
+          <div className="col-span-3 row-span-3 col-start-4 row-start-4 "> <img src="/assets/45.png"className="object-cover w-full h-full" alt="" /></div>
+      </div>
+      </Section>
     </>
   );
 }
